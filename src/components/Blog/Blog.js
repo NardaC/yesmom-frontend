085f715dca9456401a519d0deb1cafaf0fcdb60b
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2020-11-05 20:28:37 -0500

    :iphone responsive blog general

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -15,73 +15,75 @@ const Blog = () => {
         <h4 className="text-title-blog">Blog</h4>
         <img src={ondas} alt="ondas" className="ondas" />
       </div>
-      <h4 className="subtitle-dark-blog">
-        En este espacio compartiremos algunos tips que te servirán para cuidar y
-        engreir mucho más a tu bebé
+      <div className="box-blog-general">
+        <h4 className="subtitle-dark-blog">
+          En este espacio compartiremos algunos tips que te servirán para cuidar y
+          engreir mucho más a tu bebé
       </h4>
-      <div className="box-card-group">
-        <Container>
-          <CardDeck>
-            <Card className=" card-blog">
-              <Card.Img variant="top" src={blog1} />
-              <Card.Body>
-                <Card.Title>Tips para tu baby shower virtual</Card.Title>
-                <Card.Text>
-                  Te damos estos consejos para que todo salga increíble.
+        <div className="box-card-group">
+          <Container>
+            <CardDeck>
+              <Card className=" card-blog">
+                <Card.Img variant="top" src={blog1} />
+                <Card.Body>
+                  <Card.Title>Tips para tu baby shower virtual</Card.Title>
+                  <Card.Text>
+                    Te damos estos consejos para que todo salga increíble.
                 </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <a href="/blog-especific">Leer más &#8594;</a>
-              </Card.Footer>
-            </Card>
-            <Card className=" card-blog">
-              <Card.Img variant="top" src={blog1} />
-              <Card.Body>
-                <Card.Title>Artículos básicos para tu baby shower</Card.Title>
-                <Card.Text>
-                  ¿Estás por celebrar tu baby shower y no sabes qué artículos
-                  poner en tu listade regalos? Nosotros te ayudamos.
+                </Card.Body>
+                <Card.Footer>
+                  <a href="/blog-especific">Leer más &#8594;</a>
+                </Card.Footer>
+              </Card>
+              <Card className=" card-blog">
+                <Card.Img variant="top" src={blog1} />
+                <Card.Body>
+                  <Card.Title>Artículos básicos para tu baby shower</Card.Title>
+                  <Card.Text>
+                    ¿Estás por celebrar tu baby shower y no sabes qué artículos
+                    poner en tu listade regalos? Nosotros te ayudamos.
                 </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <a href="/blog-especific">Leer más &#8594;</a>
-              </Card.Footer>
-            </Card>
-            <Card className=" card-blog">
-              <Card.Img variant="top" src={blog1} />
-              <Card.Body>
-                <Card.Title>Mi bebé y su maleta para la clínica</Card.Title>
-                <Card.Text>
-                  ¿Sabes qué cosas vas a necesitar cuando vayas a la clínica con
-                  tu bebé?. Te dejamos algunas ideas que pueden servirte en esos
-                  momentos de indecisión.
+                </Card.Body>
+                <Card.Footer>
+                  <a href="/blog-especific">Leer más &#8594;</a>
+                </Card.Footer>
+              </Card>
+              <Card className=" card-blog">
+                <Card.Img variant="top" src={blog1} />
+                <Card.Body>
+                  <Card.Title>Mi bebé y su maleta para la clínica</Card.Title>
+                  <Card.Text>
+                    ¿Sabes qué cosas vas a necesitar cuando vayas a la clínica con
+                    tu bebé?. Te dejamos algunas ideas que pueden servirte en esos
+                    momentos de indecisión.
                 </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <a href="/blog-especific">Leer más &#8594;</a>
-              </Card.Footer>
-            </Card>
-            <Card className=" card-blog">
-              <Card.Img variant="top" src={blog1} />
-              <Card.Body>
-                <Card.Title>El cuarto soñado para mi bebé</Card.Title>
-                <Card.Text>
-                  Faltan pocos meses para recibir al nuevo integrante de la
-                  familia y tú todavía estás dándole vuelta a la decoración de
-                  su dormitorio. Te ayudamos con algunas ideas.
+                </Card.Body>
+                <Card.Footer>
+                  <a href="/blog-especific">Leer más &#8594;</a>
+                </Card.Footer>
+              </Card>
+              <Card className=" card-blog">
+                <Card.Img variant="top" src={blog1} />
+                <Card.Body>
+                  <Card.Title>El cuarto soñado para mi bebé</Card.Title>
+                  <Card.Text>
+                    Faltan pocos meses para recibir al nuevo integrante de la
+                    familia y tú todavía estás dándole vuelta a la decoración de
+                    su dormitorio. Te ayudamos con algunas ideas.
                 </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <a href="/blog-especific">Leer más &#8594;</a>
-              </Card.Footer>
-            </Card>
-          </CardDeck>
-        </Container>
+                </Card.Body>
+                <Card.Footer>
+                  <a href="/blog-especific">Leer más &#8594;</a>
+                </Card.Footer>
+              </Card>
+            </CardDeck>
+          </Container>
+        </div>
       </div>
       <div className="box-green-blog">
-        <img src={fondo} alt="" className="w-100" />
+        <img src={fondo} alt="" className="w-100 box-green-blog-img-fondo" />
         <h6 className="text-bottom-blog">
-          para el mundo eres una mamá,<br></br>
+          Para el mundo eres una mamá,<br></br>
           <b>para tus hijos eres el mundo</b>
         </h6>
       </div>
